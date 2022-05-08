549a89b75832f4c5d7f59e4e1ff714d2ea94eba7
LouisDelignac <102798850+LouisDelignac@users.noreply.github.com>
2022-05-08 10:24:44 +0200

    Update dashapp.py

--- dashapp.py
+++ dashapp.py
@@ -8,8 +8,8 @@ from dash import dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-server = Flask(name)
-app = dash.Dash(name, server = server, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.SKETCHY])
+server = Flask(__name__)
+app = dash.Dash(__name__, server = server, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.SKETCHY])
 
 # styling the sidebar
 SIDEBAR_STYLE = {
