5c2b6e8ceffe6b0958240b5cb45c00b2b20c05fe
BaptisteGERBOUIN <102798518+BaptisteGERBOUIN@users.noreply.github.com>
2022-05-18 12:38:29 +0200

    Animation.py

--- model/test.py
+++ model/test.py
@@ -2,67 +2,82 @@ import dash  # use Dash version 1.16.0 or higher for this app to work
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 
-df = pd.read_csv('data.csv', sep=';')
+
+df = pd.read_csv('model/data.csv', sep=';')
+#dff = df[['Station', 'Year', 'Ntot', 'Mtot']]
+#df_agreg = dff[['Station', 'Year', 'Ntot', 'Mtot']].groupby(by=['Station', 'Year']).sum()
+#print(df_agreg)
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
 app.layout = html.Div([
-    dcc.Dropdown(id='dpdn2', value= ['Josbaig'], multi=True,
+    dcc.Dropdown(id='dpdn2', value=['Papillon','Josbaig'], multi=True,
                  options=[{'label': x, 'value': x} for x in
-                          df['Station'].unique()]),
+                          df.Station.unique()]),
     html.Div([
-        dcc.Graph(id='pie-graph'),
-        dcc.Graph(id='my-graph')
+        dcc.Graph(id='pie-graph', figure={}, className='six columns'),
+        dcc.Graph(id='my-graph', figure={}, clickData=None, hoverData=None, # I assigned None for tutorial purposes. By defualt, these are None, unless you specify otherwise.
+                  config={
+                      'staticPlot': False,     # True, False
+                      'scrollZoom': True,      # True, False
+                      'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
+                      'showTips': False,       # True, False
+                      'displayModeBar': True,  # True, False, 'hover'
+                      'watermark': True,
+                      # 'modeBarButtonsToRemove': ['pan2d','select2d'],
+                        },
+                  className='six columns'
+                  )
     ])
 ])
 
 
 @app.callback(
-    Output('my-graph', 'figure'),
-    Input('dpdn2', 'value'),
+    Output(component_id='my-graph', component_property='figure'),
+    Input(component_id='dpdn2', component_property='value'),
 )
-def update_graph(value):
-    print(value)
-    mask = df['Station'] == value
-    dff = df[mask]
-    print(dff)
-    fig = px.line(dff, x='Year', y='Ntot', color='Station')
-    #fig.update_traces(mode='lines+markers')
+def update_graph(country_chosen):
+    dff2 = df[df.Station.isin(country_chosen)]
+    fig = px.line(data_frame=dff2, x='Year', y='Ntot', color='Station',
+                  custom_data=['Station', 'Mtot', 'oneacorn', 'Altitude'])
+    fig.update_traces(mode='lines+markers')
     return fig
 
 
 # Dash version 1.16.0 or higher
 @app.callback(
-    Output('pie-graph', 'figure'),
-    Input('my-graph', 'hoverData'),
-    Input('my-graph', 'clickData'),
-    Input('my-graph', 'selectedData'),
-    Input('dpdn2', 'value')
+    Output(component_id='pie-graph', component_property='figure'),
+    Input(component_id='my-graph', component_property='hoverData'),
+    Input(component_id='my-graph', component_property='clickData'),
+    Input(component_id='my-graph', component_property='selectedData'),
+    Input(component_id='dpdn2', component_property='value')
 )
-def update_side_graph(hov_data, clk_data, slct_data, value):
+def update_side_graph(hov_data, clk_data, slct_data, country_chosen):
     if hov_data is None:
-        dff2 = df[df['Station']==value]
-        dff2 = dff2[dff2['Year'] == 2011]
-        #print(dff2)
-        fig2 = px.pie(dff2, values='Mtot', names='Station',
-                      title='Mtot for 2011')
+        dff3 = df[df.Station.isin(country_chosen)]
+        print(dff3)
+        dff3 = dff3[dff3.Station == 2011]
+        print(dff3)
+        fig2 = px.pie(data_frame=dff3, values='Mtot', names='Station',
+                      title='Population for 2011')
         return fig2
     else:
-        # print(f'hover data: {hov_data}')
+        #print(f'hover data: {hov_data}')
         # print(hov_data['points'][0]['customdata'][0])
         # print(f'click data: {clk_data}')
         # print(f'selected data: {slct_data}')
-        dff2 = df[df['Station']==value]
+        dff3 = df[df.Station.isin(country_chosen)]
         hov_year = hov_data['points'][0]['x']
-        dff2 = dff2[dff2['Year'] == hov_year]
-        fig2 = px.pie(dff2, values='Mtot', names='Station')
+        dff3 = dff3[dff3.Year == hov_year]
+        fig2 = px.pie(data_frame=dff3, values='Mtot', names='Station', title=f'Population for: {hov_year}')
         return fig2
 
 
 if __name__ == '__main__':
     app.run_server()
+
